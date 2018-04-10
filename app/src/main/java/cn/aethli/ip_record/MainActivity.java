e6a185fc1b9f05e96f3d208a2859c7e1c244af1c
AethLi <aethli@aethli.cn>
2018-04-10 21:33:02 +0800

    switch_add

--- app/src/main/java/cn/aethli/ip_record/MainActivity.java
+++ app/src/main/java/cn/aethli/ip_record/MainActivity.java
@@ -28,8 +28,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView=findViewById(R.id.main_image);
-        imageView.setImageResource(R.drawable.mps);
         dt=new Dbtools(this,"Ip.db",null,1);
         sdb=dt.getWritableDatabase();
     }
